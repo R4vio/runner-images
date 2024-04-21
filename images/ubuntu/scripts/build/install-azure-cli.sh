f6395743ef28622ef4d3417a5ad1bfe2fb525574
R4vio <55717756+R4vio@users.noreply.github.com>
2024-04-21 16:07:18 +0200

    removed sudo and better versioning

--- images/ubuntu/scripts/build/install-azure-cli.sh
+++ images/ubuntu/scripts/build/install-azure-cli.sh
@@ -6,17 +6,17 @@
 ################################################################################
 
 # Download the installation script
-curl -fsSL https://aka.ms/InstallAzureCLIDeb -o install-azure-cli.sh
+curl -fsSL https://aka.ms/InstallAzureCLIDeb -o install-azure-cli-script.sh
 
 # Modify the script as necessary
 # Example: Changing the installation to a specific version of Azure CLI
-sed -i 's/sudo apt-get install -y azure-cli/sudo apt-get install -y azure-cli=2.58.0/g' install-azure-cli.sh
+sed -i 's/apt-get install -y azure-cli/apt-get install -y azure-cli=2.58.0-1~focal/g' install-azure-cli-script.sh
 
 # Make the script executable
-chmod +x install-azure-cli.sh
+chmod +x install-azure-cli-script.sh
 
 # Execute the modified script
-sudo ./install-azure-cli.sh
+sudo ./install-azure-cli-script.sh
 
 # Add to helper scripts (if still necessary)
 echo "azure-cli https://docs.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt" >> $HELPER_SCRIPTS/apt-sources.txt
